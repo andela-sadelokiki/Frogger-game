7a40ec3fe93f75b9e796b9284d2e0286556ed241
andela-sadelokiki <susan.adelokiki@andela.co>
2015-04-02 12:07:51 +0100

    Initializing repo for gh-pages

---
+++ js/engine.js
@@ -0,0 +1,126 @@
+var score = 0;
+
+var Engine = (function(global) {
+    
+    var doc = global.document,
+        win = global.window,
+        canvas = doc.createElement('canvas'),
+        ctx = canvas.getContext('2d'),
+        lastTime;
+        document.getElementById('score').innerHTML= "Score: " + score;
+        canvas.width = 505;
+        canvas.height = 606;
+        doc.body.appendChild(canvas);
+
+    
+    function main() {
+       
+        var now = Date.now(),
+            dt = (now - lastTime) / 1000.0;
+
+        update(dt);
+        render();
+        waterCollisions();
+        lastTime = now;
+        win.requestAnimationFrame(main);
+
+    };
+
+   
+    function init() {
+        
+        lastTime = Date.now();
+        main();
+
+    }
+
+   
+    function update(dt) {
+
+        updateEntities(dt);
+        checkCollisions();
+
+    }
+
+    
+    function updateEntities(dt) {
+
+        allEnemies.forEach(function(enemy) {
+            enemy.update(dt);
+        });
+        player.update();
+
+    }
+
+    function startover(){
+
+        player.x = 200;
+        player.y = 400;
+        updateX = 200;
+        updateY = 400;
+        
+    }
+
+    function waterCollisions(){
+        if(player.y === 68){
+        score+=10;
+        document.getElementById('score').innerHTML= "Score: " + score;
+        startover();
+    }
+
+    }
+
+    function checkCollisions(){
+        allEnemies.forEach(function(enemy) {
+             if(enemy.x <= player.x && enemy.x >= player.x - 65   && enemy.y === player.y){
+                if (score>0){
+                  score-=5;
+                }
+                document.getElementById('score').innerHTML= "Score: " + score;
+                startover();   
+        }    
+        });
+    }
+
+    function render() {
+      
+        var rowImages = [
+                'images/water-block.png',   // Top row is water
+                'images/stone-block.png',   // Row 1 of 3 of stone
+                'images/stone-block.png',   // Row 2 of 3 of stone
+                'images/stone-block.png',   // Row 3 of 3 of stone
+                'images/grass-block.png',   // Row 1 of 2 of grass
+                'images/grass-block.png'    // Row 2 of 2 of grass
+            ],
+            numRows = 6,
+            numCols = 5,
+            row, col;
+
+        for (row = 0; row < numRows; row++) {
+            for (col = 0; col < numCols; col++) {
+                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+            }
+        }
+        renderEntities();
+
+    }
+
+    function renderEntities() {
+        
+        allEnemies.forEach(function(enemy) {
+            enemy.render();
+        });
+        player.render();
+
+    }
+    
+    Resources.load([
+        'images/stone-block.png',
+        'images/water-block.png',
+        'images/grass-block.png',
+        'images/enemy-bug.png',
+        'images/char-boy.png'
+    ]);
+    Resources.onReady(init);
+    global.ctx = ctx;
+})(this);
